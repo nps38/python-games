b448f3b5c04277ca24ae024d25fa7dffdd6f8eff
Neha Shirwalker <97172302+nps38@users.noreply.github.com>
2024-06-24 03:55:57 -0400

    Add files via upload

---
+++ geometric-designs/spiral.py
@@ -0,0 +1,23 @@
+from turtle import *
+import random
+
+def randomColorGenerator():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+def circularPage():
+    speed(100)
+    width(1)
+    colormode(255)
+    bgcolor("black")
+
+    color = randomColorGenerator()
+    
+    for y in range(240):
+        pencolor(color)
+        circle(y)
+        left(5)
+        
+circularPage()
